1580aaa687811f24d1a890a8862c3bdadfb66260
Gollavilli <gkchaitanya2@gmail.com>
2025-05-10 12:55:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -31,17 +31,8 @@ $(document).ready(function() {
       const tableBody = document.getElementById('tableBody');
       tableBody.innerHTML = '';
       
-      // Sort components to put special ones at the top
-      const sortedComponents = [...data.components].sort((a, b) => {
-        // First put special components at the top
-        if (a.special && !b.special) return -1;
-        if (!a.special && b.special) return 1;
-        // Then sort alphabetically
-        return a.name.localeCompare(b.name);
-      });
-      
       // Populate table with data
-      sortedComponents.forEach(component => {
+      data.components.forEach(component => {
         const row = document.createElement('tr');
         
         // Add special highlighting if needed
